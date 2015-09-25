567944384f0f6d72bc97785d76a7a2b06052a5e5
nicholashaynes1 <nicholashaynes1@gmail.com>
2015-09-25 13:09:38 -0600

    Finished by adding function to change background color and button color.

--- app/src/main/java/ctec/stupidproject/StupidActivity.java
+++ app/src/main/java/ctec/stupidproject/StupidActivity.java
@@ -4,20 +4,41 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.RelativeLayout;
+import android.view.View;
+import android.graphics.Color;
+public class StupidActivity extends ActionBarActivity
+{
+    private Button colorChangeButton;
+    private RelativeLayout background;
+    private TextView sillyWords;
 
-public class StupidActivity extends ActionBarActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stupid);
+
+        colorChangeButton = (Button) findViewById(R.id.sillyButton);
+        background = (RelativeLayout) findViewById(R.id.appBackground);
+        sillyWords = (TextView) findViewById(R.id.sillyWords);
+
+        setupListeners();
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_stupid, menu);
         return true;
+
+
     }
 
     @Override
@@ -32,6 +53,41 @@ public class StupidActivity extends ActionBarActivity {
             return true;
         }
 
-        return super.onOptionsIt+emSelected(item);
+        return super.onOptionsItemSelected(item);
     }
+
+    private void changeColors()
+    {
+        int redColor;
+        int greenColor;
+        int blueColor;
+
+        redColor = (int) (Math.random() * 256);
+        greenColor = (int) (Math.random() * 256);
+        blueColor = (int) (Math.random() * 256);
+
+        background.setBackgroundColor(Color.rgb(redColor, greenColor, blueColor));
+
+
+        redColor = (int) (Math.random() * 256);
+        greenColor = (int) (Math.random() * 256);
+        blueColor= (int) (Math.random() * 256);
+
+        colorChangeButton.setBackgroundColor(Color.rgb(redColor, greenColor, blueColor));
+    }
+
+     private void setupListeners()
+     {
+         colorChangeButton.setOnClickListener(new View.OnClickListener()
+         {
+             @Override
+         public void onClick(View ButtonView)
+             {
+                 //This is where you put code that happens when you click a button.
+                 changeColors();
+             }
+         });
+
+
+     }
 }
